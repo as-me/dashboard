293d3949802403d354a60159ce9668ebb1169279
Sanjay krishna anbalagan <sanjay1909@gmail.com>
2015-09-09 17:38:08 -0400

    Layout Children management Implemented

--- src/Layout.js
+++ src/Layout.js
@@ -8,57 +8,61 @@ var ToolPanel = require('./components/ToolPanel.js');
 
 class Layout extends React.Component {
 
-    constructor(props) {
-        super(props)
-        this.tools = window.NavigationHashMap.getObject("tools");
+        constructor(props) {
+            super(props)
+            this.tools = window.NavigationHashMap.getObject("tools");
 
-        this.state = {
-            names: this.tools.getNames()
-        };
-        this._updateState = this._updateState.bind(this);
-    }
+            this.state = {
+                names: this.tools.getNames()
+            };
+            this._updateState = this._updateState.bind(this);
+        }
 
-    componentDidMount() {
-        this.tools.addGroupedCallback(this, this._updateState);
-    }
+        componentDidMount() {
+            //this.tools.addGroupedCallback(this,this._updateState );
+            this.tools.childListCallbacks.addImmediateCallback(this, this._updateState)
+        }
 
-    componentDidUpdate(prevProps, prevState) {
+        componentDidUpdate(prevProps, prevState) {
 
 
-    }
+        }
 
-    _updateState() {
-        //this wil call render function which in turn calls componentDidUpdate
-        this.setState({
-            names: this.tools.getNames()
-        });
-    }
+        _updateState() {
+            this.setState({
+                names: this.tools.getNames()
+            });
+        }
 
-    componentWillUnmount() {
-        this.tools.removeCallback(this, this._updateState)
-    }
-    render() {
-        var children = [];
-        if (this.state.names) {
-            for (var i = 0; i < this.state.names.length; i++) {
-                var ls = this.state.names[i];
-                var toolName = this.tools.getObject(ls).value;
-                var toolContent = "Sessioned Chart Component will be tied up insted of " + toolName;
-                children.push( < ToolPanel title = {
-                        toolName
-                    }
-                    content = {
-                        toolContent
-                    }
-                    />
-                );
+        componentWillUnmount() {
+            this.tools.childListCallbacks.removeCallback(this, this._updateState)
+        }
+        render() {
+            var children = [];
+            if (this.state.names) {
+                for (var i = 0; i < this.state.names.length; i++) {
+                    var ls = this.state.names[i];
+                    var tool = this.tools.getObject(ls)
+                    var toolName = tool.value;
+                    var toolContent = "Sessioned Chart Component will be tied up insted of " + toolName;
+                    children.push( < ToolPanel title = {
+                            toolName
+                        }
+                        content = {
+                            toolContent
+                        }
+                        sessionedTool = {
+                            tool
+                        }
+                        />
+                    );
+                }
+            }
+
+            return ( < Grid > {
+                    children
+                } < /Grid>);
             }
         }
 
-        return ( < Grid > {
-            children
-        } < /Grid>);
-    }
-}
-
-module.exports = Layout;
+        module.exports = Layout;
--- src/components/ToolPanel.js
+++ src/components/ToolPanel.js
@@ -2,13 +2,42 @@ import * as React from 'react';
 import * as ReactBootstrap from 'react-bootstrap';
 
 var Panel = ReactBootstrap.Panel;
+var Button = ReactBootstrap.Button;
 
 
 
 class ToolPanel extends React.Component {
+    constructor(props) {
+        super(props)
+        this.tool = props.sessionedTool;
+
+
+        this._closePanel = this._closePanel.bind(this);
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+
+
+    }
+
+    _closePanel() {
+        var tools = window.NavigationHashMap.getObject("tools");
+        var name = tools.getName(this.props.sessionedTool)
+        tools.removeObject(name);
+    }
+
+    componentWillUnmount() {}
     render() {
-        return ( < Panel header = {
-                this.props.title
+        return ( < Panel header = { < div > {
+                    this.props.title
+                } < Button bsSize = 'small'
+                onClick = {
+                    this._closePanel
+                } > < i className = "fa fa-times fa-lg" > < /i></Button > < /div >
             } > {
                 this.props.content
             } < /Panel>
